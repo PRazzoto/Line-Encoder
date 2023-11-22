9135418a7e913b20208e51239d4688cccba43919
Patrick Razzoto <patrickrazzoto@gmail.com>
2023-11-22 10:42:50 -0300

    binary conversion functions created

---
+++ binary_func.py
@@ -0,0 +1,33 @@
+def stringToBin(string):
+    binary_result = ""
+
+    for char in string:
+        # Pega o Unicode e converte para binário, removendo o 0b
+        string_binary = bin(ord(char))[2:].zfill(8)
+
+        binary_result += string_binary
+
+    return binary_result
+
+
+input_string = "Café"
+print(stringToBin(input_string))
+
+
+def binToString(binary_string):
+    # Dividindo a string em chunks de 8 bits
+    chunks = [binary_string[i : i + 8] for i in range(0, len(binary_string), 8)]
+
+    # Converte para decimal cada chunk de 8 bits
+    decimal = [int(chunk, 2) for chunk in chunks]
+
+    # Converte cada int para o seu correspondente em ASCII
+    letters = [chr(value) for value in decimal]
+
+    final_string = "".join(letters)
+
+    return final_string
+
+
+bin_string = "01100011011000010110011011101001"
+print(binToString(bin_string))
